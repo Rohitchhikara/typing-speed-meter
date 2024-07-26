5667e455b7ceb3368b7b46e773d532c24f9bdf2b
404 <102517230+Rohitchhikara@users.noreply.github.com>
2024-07-26 16:28:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import time
 
-start_time = time.time() # save the current time
-title = "the quick brown fox jumped over the lazy dog"
-type = input("type \"the quick brown fox jumped over the lazy dog\" : ")
+main_line = ["the quick brown fox jumped over the lazy dog"]
+def heading():
+    print("\n---------typeing test -------- \n ", main_line)
+    print("you have to type this line above ^ and press enter")
+    heaing_input = input("`y` to start , `q` to quit , 'e' to change line : ")
+    if heaing_input == "y":
+        print("start in 3 sec")
+        print(3)
+        time.sleep(1)
+        print(2)
+        time.sleep(1)
+        print(1)
+        metying()  # corrected function name
+    if heaing_input == "q":
+        exit()
 
-end_time = time.time() # save the current time again after the code has run
+    if heaing_input == 'e':
+        line = input("enter the line you want to set: ")
+        if line != "":
+            main_line[0] = line
+        heading()
+    else:
+        print("no option found")
+        heading()
 
-elapsed_time = end_time - start_time # calculate the elapsed time
 
-if title == ("the quick brown fox jumped over the lazy dog"):
-    print("you type correct")
-    print("Elapsed time: ", elapsed_time, "seconds")
-else:
-    print("you type wrong word")
+def metying():
+    start_time = time.time()  # save the current time
+    print(main_line)
+    type = input(": ")
+    end_time = time.time()  # save the current time again after the code has run
+    elapsed_time = end_time - start_time  # calculate the elapsed time
+    if type == main_line[0]:  # corrected indexing
+        print("you type correct")
+        print("Elapsed time: ", elapsed_time, "seconds")
+        heading()
+    else:
+        print("you type wrong word")
+        heading()
 
-
+heading()
+def exit():
+    pass
